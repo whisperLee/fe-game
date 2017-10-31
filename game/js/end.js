fa2f8a4050d12d3dd00682c3a525eb9adaf145b1
linlin <linlin@yujian.com>
2017-10-31 17:38:50 +0800

    add

--- game/js/end.js
+++ game/js/end.js
@@ -1,31 +1,39 @@
 /**
  * Created by nielinlin on 2017/9/16.
  */
+Vue.component('userwin',component.user)
+Vue.component('userlose',component.user)
 new Vue({
     el: '#end',
     data: {
+        userwin:[
+            {"userId":101,"nickName":"aaa","number":1,"sex":1,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":102,"nickName":"bbb","number":2,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":103,"nickName":"ccc","number":3,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":104,"nickName":"ddd","number":4,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":105,"nickName":"eee","number":5,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":106,"nickName":"fff","number":6,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":107,"nickName":"ggg","number":7,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":108,"nickName":"xxx","number":8,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":109,"nickName":"zzz","number":9,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}}
+            ],
+        userlose:[
+            {"userId":106,"nickName":"fff","number":6,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":107,"nickName":"ggg","number":7,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":108,"nickName":"xxx","number":8,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}},
+            {"userId":109,"nickName":"zzz","number":9,"stage":1,"star":1,"personalInfo":{"identityName":"猎人","identityId":5}}]
     },
     created: function () {
         var _self = this
-        _self.check_in()
+        //_self.active()
     },
     methods: {
-        check_in: function () {
-            var _self = this
-            var shopId = global.urlHash().shopId || 0
-            var d = {
-                url: 'game/play/checkin',
-                data: {shopId: shopId},
-                success: function (d) {
-                    if (d.status.code === 'OK') {
-                        global.pop_tips('签到成功')
-                    } else {
-                        global.pop_tips('签到失败，请重新扫码签到！',global.saoyisao)
-
-                    }
-                }
-            }
-            global.ajax(d)
+        mvp: function (event) {
+            var el = $(event.currentTarget)
+            el.addClass('zaned')
+            //animate.zan(el,function(){
+            el.closest('.win').find('.zan').removeClass('zan')
+            //})
         }
     }
 });

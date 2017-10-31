bf032d9bfc40427c5b3415609b27b4969e8c9cf8
linlin <linlin@yujian.com>
2017-10-31 23:56:52 +0800

    home modify layer

--- game/data.js
+++ game/data.js
@@ -321,11 +321,61 @@ DIE_SPEAK
 
 {"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT","eventInfo":{"eventType":"EVENT_WEREWOLF","eventName":"狼刀","eventDesc":"选择一位玩家刀掉,所有狼人必须统一意见,否则视为空刀.","chooseCount":1,"unifyFlag":true,"eventSurplusTime":10}}
 
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT","eventInfo":{"eventType":"EVENT_SEER","eventName":"预言家验人","eventDesc":"选择一位玩家查验其身份.","chooseCount":1,"eventSurplusTime":6}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT","eventInfo":{"eventType":"EVENT_WITCH","witchStatusEnum":"PAPA","eventName":"女巫救人/毒人","eventDesc":"昨天狼队空刀,是否使用毒药,要毒谁?","chooseCount":1,"eventSurplusTime":6}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT","eventInfo":{"eventType":"EVENT_SAVIOR","eventName":"守卫护盾","eventDesc":"选择一位玩家守护,保证其中刀不死.","chooseCount":1,"eventSurplusTime":6}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"UPDATE_AND_EVENT","gameInfo":{"gameStatus":"EXILE_VOTE","gameStatusStr":"放逐投票"},"eventInfo":{"eventType":"EVENT_EXILE_VOTE","eventName":"放逐投票","eventDesc":"请选择你要放逐的玩家.","chooseCount":1,"eventSurplusTime":10}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"UPDATE_LEADER","leaderInfo":{"leaderFlag":true,"leaderButton":["EMPTY"]}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT_RESULT","eventResultInfo":{"eventDesc":"你决定弃票.","targetNumber":"0","resultSurplusTime":3}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"UPDATE","gameInfo":{"gameStatus":"EXILE_RESULT","gameStatusStr":"放逐结果","voteInfoList":[{"voteTitle":"放逐投票","voteDetailList":[{"votedNumber":0,"voteNumberList":[1,2,3,4,6,7,9]}]}],"voteShowType":"LAST"},"showMsg":"所有玩家都弃票,今天没有人被放逐."}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT_RESULT","eventResultInfo":{"eventType":"EVENT_WEREWOLF","eventDesc":"狼队,你们今晚刀掉的是'4'号,狼人请闭眼.","targetNumber":"4"}}
+
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT_RESULT","eventResultInfo":{"eventDesc":"女巫,你放弃了操作.","targetNumber":"0","resultSurplusTime":3}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"UPDATE_AND_EVENT","userInfoList":[{"number":7,"identityStatus":"DEAD"}],"gameInfo":{"gameStatus":"CHANGE_SERGEANT","gameStatusStr":"移交警长"},"eventInfo":{"eventName":"移交警徽","eventDesc":"请选择一位玩家将警徽移交给他.","chooseCount":1}}
+
+{"statusResponse":{"retcode":2001,"code":"WebSocketError","msg":"当前不是发言阶段,不能结束发言.","requestUrl":"/gameUser/receiveUserEvent"},"msgType":"EVENT_OK"}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT_RESULT","eventResultInfo":{"eventDesc":"你要放逐3号玩家.","targetNumber":"3","resultSurplusTime":3}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"UPDATE","gameInfo":{"gameStatus":"EXILE_RESULT","gameStatusStr":"放逐结果","voteInfoList":[{"voteTitle":"放逐投票","voteDetailList":[{"votedNumber":3,"voteNumberList":[1]},{"votedNumber":0,"voteNumberList":[2,3,4,6,7,9]}]}],"voteShowType":"LAST"},"showMsg":"3号玩家被放逐.投票详情可以在手机上查看."}
+
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT","gameInfo":{"nightFlag":false,"gameTime":1,"gameStatus":"CAMPAIGN_SERGEANT","gameStatusStr":"竞选警长"},"eventInfo":{"eventType":"EVENT_CAMPAIGN_SERGEANT","eventName":"警长竞选","eventDesc":"准备竞选警长,请选择是否参选.","chooseCount":0,"eventSurplusTime":10}}
+
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT_RESULT","eventResultInfo":{"eventDesc":"你选择了上警.","resultSurplusTime":3}}
+
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"UPDATE","userInfoList":[{"number":1,"campaignFlag":true},{"number":2,"campaignFlag":true},{"number":7,"campaignFlag":true}],"gameInfo":{"gameStatus":"CAMPAIGN_SPEAK","gameStatusStr":"竞选警长发言"},"showMsg":"1号,2号,7号玩家参与竞选.请从'2'号玩家开始,'逆'序发言.2->1->7"}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT","eventInfo":{"eventType":"EVENT_SAVIOR","eventName":"守卫护盾","eventDesc":"选择一位玩家守护,保证其中刀不死.","chooseCount":1,"eventSurplusTime":6}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT_RESULT","eventResultInfo":{"eventDesc":"守卫,你守护的是'3'号,守卫请闭眼.","targetNumber":"3","resultSurplusTime":3}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT_RESULT","eventResultInfo":{"eventDesc":"守卫,你守护的是'4'号,守卫请闭眼.","targetNumber":"4","resultSurplusTime":3}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT","gameInfo":{"nightFlag":false,"gameTime":1,"gameStatus":"CAMPAIGN_SERGEANT","gameStatusStr":"竞选警长"},"eventInfo":{"eventType":"EVENT_CAMPAIGN_SERGEANT","eventName":"警长竞选","eventDesc":"准备竞选警长,请选择是否参选.","chooseCount":0,"eventSurplusTime":10}}
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"UPDATE","userInfoList":[{"userId":107,"number":7,"sergeantFlag":true,"campaignFlag":false}],"gameInfo":{"gameStatus":"CAMPAIGN_RESULT","gameStatusStr":"竞选结果"},"showMsg":"只有7号玩家参选,'7'号玩家自动当选警长.拥有1.5票归票权."}
+
+
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"UPDATE","userInfoList":[{"number":7,"identityStatus":"DEAD"}],"gameInfo":{"gameStatus":"CHANGE_SERGEANT","gameStatusStr":"移交警长"}}
 
 
 
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"EVENT","eventInfo":{"eventType":"EVENT_SEER","eventName":"预言家验人","eventDesc":"选择一位玩家查验其身份.","chooseCount":1,"eventSurplusTime":6}}
 
 
+{"statusResponse":{"retcode":0,"code":"OK"},"msgType":"UPDATE","userInfoList":[{"number":7,"identityStatus":"DEAD"}],"gameInfo":{"gameStatus":"CHANGE_SERGEANT","gameStatusStr":"移交警长"}}
 
 
 

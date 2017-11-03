87d7de0dff95701e1aad34a03f31688937c22e83
whisperLee <lsp49498632@gmail.com>
2017-11-03 17:40:27 +0800

    Add files via upload

--- game/js/end.js
+++ game/js/end.js
@@ -118,39 +118,115 @@ new Vue({
             "number": 1,
             "identityId": 2,
             "identityName": "狼人",
+
             "changeLevelFlag": false,
             "upFlag": false,
-            "oldScore": 0,
-            "newScore": 0,
-            "thisNeedScore": 0,
-            "thisStar": 3,
+            "oldScore": 60,
+            "newScore": 90,
+            "thisNeedScore": 50,
+            "thisStar": 2,
             "thisStage": 1,
             "nextNeedScore": 100,
-            "nextStar": 2,
+            "nextStar":3,
             "nextStage": 1,
+            "preNeedScore": 0,
+            "preStar": 1,
+            "preStage": 1,
+
             "endTime": "2017-10-23 00:53",
             "costTime": "02:48",
             "gameStatus": "GOOD_WIN",
             "gameStatusStr": "好人阵营胜利",
-            "mvpFlag": true
+            "mvpFlag": false
         }
     },
     created: function () {
         var _self = this
-        _self.userAccountChange()
+        setTimeout(function(){
+            //
+            //setTimeout(function(){
+            _self.userAccountChange()
+            //},1000)
+            _self.mvp(203)
+        },1000)
+
     },
     methods: {
-        mvp: function (event) {
+        voteMvp: function (event) {
             var el = $(event.currentTarget)
             if(el.hasClass('zan')){
                 el.addClass('zaned')
                 el.closest('.win').find('.zan').removeClass('zan')
             }
         },
+        mvp:function(num){
+            var _self = this
+            $('.gameEnd .result .userList li[userid="'+num+'"]').addClass('mvp')
+        },
         userAccountChange: function(){
-            setTimeout(function(){
-                animate.layerEnter($('.userAccount'))
-            },3000)
+            var _self = this
+            var el = $('.userAccount')
+            var d = _self.personalAccountInfo
+            var oldPre,newPre
+            var _per = el.find('.pro b')
+
+
+
+            newPre = (d.newScore-d.thisNeedScore)*100/(d.nextNeedScore-d.thisNeedScore)
+            newPre = newPre*0.8+10 // 样式的问题，当前星级在整个进度条的80%，所以在*0.8之后，加上上一星级的10%
+            console.log(newPre)
+            if(_self.changeLevelFlag){
+                if(upFlag){
+
+                }else{
+
+                }
+            }else{
+                oldPre = (d.oldScore-d.thisNeedScore)*100/(d.nextNeedScore-d.thisNeedScore)
+                oldPre = parseInt(oldPre*0.8)+10
+                console.log(oldPre)
+
+
+
+            }
+            _per.css({'width':oldPre+'%'})
+            animate.layerEnter($('.userAccount'))
+
+
+            _per.velocity({
+                width:newPre
+            },{
+                duration: 1000,
+                delay:3000,
+                complete: function() {
+
+                }
+            })
+
+
+
+            //if(_self.upFlag){
+            //     el.find('.pro b').victory({
+            //         width:'50%'
+            //     },{
+            //         duration: 1000,
+            //         complete: function() {
+            //
+            //         }
+            //     })
+
+            // "changeLevelFlag": false,
+            //     "upFlag": false,
+            //     "oldScore": 0,
+            //     "newScore": 0,
+            //     "thisNeedScore": 0,
+            //     "thisStar": 3,
+            //     "thisStage": 1,
+            //     "nextNeedScore": 100,
+            //     "nextStar": 2,
+            //     "nextStage": 1,
+
+            //}
         }
     }
 });

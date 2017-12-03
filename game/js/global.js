ea281f667654342ec64c7c24a280341e332e5210
linlin <linlin@yujian.com>
2017-12-03 21:01:56 +0800

    home modify layer

--- game/js/global.js
+++ game/js/global.js
@@ -71,8 +71,7 @@ var global = {
   setCookie: function (name, value) {
     var Days = 30
     var exp = new Date()
-    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000)
-    document.cookie = name + '=' + escape(value) + ';expires=' + exp.toGMTString()
+    document.cookie = name + '=' + escape(value)  + ';expires=' + exp.toGMTString()+';path=/'
   },
     saoyisao: function () {
       console.log('调去扫一扫功能')

00ddce8bf62d4db2dd9de27c06e965f911800f2f
linlin <linlin@yujian.com>
2018-05-26 17:05:41 +0800

    add

--- game/js/wx/wx_global.js
+++ game/js/wx/wx_global.js
@@ -95,7 +95,6 @@ global = $.extend({},global,{
     },
     saoyisao: function () {
         console.log('调扫一扫功能')
-        alert("sao")
         wx.scanQRCode({
             needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
             scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有

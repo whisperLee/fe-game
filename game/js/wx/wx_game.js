53ae0d4317e9b4587a14ba595ac426eaaf7df406
linlin <linlin@yujian.com>
2018-05-26 17:04:49 +0800

    add

--- game/js/wx/wx_game.js
+++ game/js/wx/wx_game.js
@@ -243,7 +243,7 @@ new Vue({
                     if (d.status.code === 'OK') {
                         global.pop_tips('退座成功', function () {
                             //_self.saoyisao()
-                            window.reload()
+                            window.location.href=window.location.href.split("?")[0] //刷新当前页面
                         })
                     } else{
                         global.codeError(d)
